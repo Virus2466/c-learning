4a03c5a8fb8fcd8e118cbbeb5e6ec7e4b30807dc
Virus2466 <Pateldev2311@protonmail.com>
2024-08-02 12:42:39 +0530

    Added C

---
+++ basics/helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+// argc and argv are command line arguments
+int main(int argc , char *argv[]) 
+
+{
+
+    printf("Hello , World");
+
+    addNumbers(3,5);
+
+    return 0;
+}
